d305ad01a2add4a1ca20321bfe74079ce6ad71d3
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-07-05 19:44:00 +0300

    Расстановка на карте / Location on the map

--- Program.java
+++ Program.java
@@ -64,61 +64,97 @@ public class Program {
             System.out.println("WINNER TEAM 2: " + hero3Team2.getInformation());
         }
 
-        // Расстановка на карте.Location on the map (Случайные герои)
-        ArrayList<Heroes> list = new ArrayList<>();
-        list.add(new Magician(Names.Magician.getName(), 0, 1));
-        list.add(new Monk(Names.Monk.getName(), 0, 2));
-        list.add(new Sniper(Names.Sniper.getName(), 0, 3));
-        list.add(new Crossbowman(Names.Crossbowman.getName(), 0, 4));
-        list.add(new Robber(Names.Robber.getName(), 0, 5));
-        list.add(new Spearman(Names.Spearman.getName(), 0, 6));
-        list.add(new Peasant(Names.Peasant.getName(), 0, 7));
-        // list.forEach(n-> System.out.println(n.getInfo()));
+        // Расстановка на карте / Location on the map
 
         System.out.println();
         System.out.println("team1:");
         int teamCount = 4;
         Random rand = new Random();
         ArrayList<Heroes> team1 = new ArrayList<>();
-        for (int i = 0; i < teamCount; i++) {
-            int val = rand.nextInt(4);
-            switch (val) {
-                case 0:
-                    team1.add(new Magician(Names.Magician.getName(), 0, val + 1));
-                    break;
-                case 1:
-                    team1.add(new Sniper(Names.Sniper.getName(), 0, val + 1));
-                    break;
-                case 2:
-                    team1.add(new Robber(Names.Robber.getName(), 0, val + 1));
-                    break;
-                case 3:
-                    team1.add(new Peasant(Names.Peasant.getName(), 0, val + 1));
-            }
+        int val = rand.nextInt(2);
+        int position = 0;
+        switch (val) {
+            case 0:
+                team1.add(new Magician(Names.Magician.getName(), 0, position));
+                break;
+            case 1:
+                team1.add(new Monk(Names.Monk.getName(), 0, position));
+                break;
+        }
+        position++;
+        val = rand.nextInt(2);
+        switch (val) {
+            case 0:
+                team1.add(new Sniper(Names.Sniper.getName(), 0, position));
+                break;
+            case 1:
+                team1.add(new Crossbowman(Names.Crossbowman.getName(), 0, position));
+                break;
+        }
+        position++;
+        val = rand.nextInt(2);
+        switch (val) {
+            case 0:
+                team1.add(new Robber(Names.Robber.getName(), 0, position));
+                break;
+            case 1:
+                team1.add(new Spearman(Names.Spearman.getName(), 0, position));
+                break;
+        }
+        position++;
+        val = rand.nextInt(2);
+        switch (val) {
+            case 0:
+                team1.add(new Peasant(Names.Peasant.getName(), 0, position));
+                break;
+            case 1:
+                team1.add(new Peasant(Names.Peasant.getName(), 0, position));
+                break;
+        }
+        for (int i = 0; i < 4; i++){
             System.out.println(team1.get(i).getInfo());
         }
+
         System.out.println();
         System.out.println("team2:");
+
         ArrayList<Heroes> team2 = new ArrayList<>();
-        for (int i = 0; i < teamCount; i++) {
-            int val = rand.nextInt(4);
-            switch (val) {
-                case 0:
-                    team2.add(new Monk(Names.Monk.getName(), 5, val + 1));
-                    break;
-                case 1:
-                    team2.add(new Crossbowman(Names.Crossbowman.getName(), 5, val + 1));
-                    break;
-                case 2:
-                    team2.add(new Spearman(Names.Spearman.getName(), 5, val + 1));
-                    break;
-                case 3:
-                    team2.add(new Peasant(Names.Peasant.getName(), 5, val + 1));
-            }
+        position = 0;
+        if (team1.get(position).getInfo() == "Gandalf") {
+            team2.add(new Monk(Names.Monk.getName(), 5, position));
+        }
+        else {
+            team2.add(new Magician(Names.Magician.getName(), 5, position));
+        }
+        position++;
+        if (team1.get(position).getInfo() == "Aragorn") {
+            team2.add(new Crossbowman(Names.Crossbowman.getName(), 5, position));
+        }
+        else {
+            team2.add(new Sniper(Names.Sniper.getName(), 5, position));
+        }
+        position++;
+        if (team1.get(position).getInfo() == "Gimli") {
+            team2.add(new Spearman(Names.Spearman.getName(), 5, position));
+        }
+        else {
+            team2.add(new Robber(Names.Robber.getName(), 5, position));
+        }
+        position++;
+        if (team1.get(position).getInfo() == "Sem") {
+            team2.add(new Peasant(Names.Peasant.getName(), 5, position));
+        }
+        else {
+            team2.add(new Peasant(Names.Peasant.getName(), 5, position));
+        }
+        for (int i = 0; i < 4; i++) {
             System.out.println(team2.get(i).getInfo());
         }
         System.out.println();
         System.out.println("Ближайший противник: ");
+        team2.forEach(n -> n.step(team1));
+        System.out.println();
         team1.forEach(n -> n.step(team2));
+
     }
 }
